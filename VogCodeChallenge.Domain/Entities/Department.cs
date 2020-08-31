5a561c097afb0a7257efddd190a938e8745b817e
olaidenojeemekeolere <olaide.ekeolere@landastudio.net>
2020-08-31 08:21:36 +0100

    Annotations added

--- VogCodeChallenge.Domain/Entities/Department.cs
+++ VogCodeChallenge.Domain/Entities/Department.cs
@@ -1,12 +1,17 @@
 ﻿
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using VogCodeChallenge.Domain.Common;
 namespace VogCodeChallenge.Domain.Entities
 {
     public class Department : BaseEntity
     {
+        [Required, StringLength(150, MinimumLength = 4,
+                                ErrorMessage = "Name cannot be longer than 150 characters")]
         public string Name { get; set; }
 
+        [Required, StringLength(150, MinimumLength = 4,
+                                ErrorMessage = "Address cannot be longer than 150 characters")]
         public string Address { get; set; }
 
         public ICollection<Employee> Employees { get; set; }
--- VogCodeChallenge.Domain/Entities/Employee.cs
+++ VogCodeChallenge.Domain/Entities/Employee.cs
@@ -5,9 +5,17 @@ namespace VogCodeChallenge.Domain.Entities
 {
     public class Employee : BaseEntity
     {
+        [Required, StringLength(50, MinimumLength = 4,
+                                ErrorMessage = "First Name cannot be longer than 50 characters")]
         public string FirstName { get; set; }
+        [Required, StringLength(50, MinimumLength = 4,
+                                ErrorMessage = "Last Name cannot be longer than 50 characters")]
         public string LastName { get; set; }
+        [Required, StringLength(100, MinimumLength = 4,
+                                ErrorMessage = "Job Title cannot be longer than 100 characters")]
         public string JobTitle { get; set; }
+        [Required, StringLength(150, MinimumLength = 4,
+                                ErrorMessage = "Address cannot be longer than 150 characters")]
         public string Address { get; set; }
 
         [Required]
