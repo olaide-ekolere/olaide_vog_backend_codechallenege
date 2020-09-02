6e0a6fefa580bfd59b984ea616d09d2d2228d837
olaidenojeemekeolere <olaide.ekeolere@landastudio.net>
2020-09-02 10:49:17 +0100

    Added null case to switch

---
+++ VogCodeChallenge.API/Classes/TESTModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace VogCodeChallenge.API.Classes
+{
+    public static class TESTModule
+    {
+        public static object Module(object testObject)
+        {
+            switch (testObject)
+            {
+                case 1:
+                case 2:
+                    return doubler((int)testObject);
+                case int value when value >= 3:
+                    return tripler(value);
+                case float floatValue when floatValue == 1.0f:
+                    return leveler(floatValue);
+                case null: //added omitted null case
+                    return 0;
+                default:
+                    return testObject;
+
+            }
+
+            int doubler(int amount) => amount * 2;
+            int tripler(int amount) => amount * 3;
+            double leveler(double amount) => amount - 0.1f * 10f;
+        }
+    }
+}
