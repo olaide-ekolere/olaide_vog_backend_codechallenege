03c1c3481f8c24411f67e718f2d80af65a9a25a0
olaidenojeemekeolere <olaide.ekeolere@landastudio.net>
2020-09-02 12:35:11 +0100

    Used foreach loop

---
+++ VogCodeChallenge.API.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VogCodeChallenge.API.ConsoleApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            QuestionClass.TestQuestion();
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ VogCodeChallenge.API.Console/QuestionClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+namespace VogCodeChallenge.API.ConsoleApp
+{
+    public class QuestionClass
+    {
+        static List<string> NamesList = new List<string>() {
+            "Jimmy",
+            "Jeff"
+        };
+
+        public static void TestQuestion()
+        {
+            foreach(string Name in NamesList)
+            {
+                Console.WriteLine(Name);
+            }
+            /*
+            using(var enumator = NamesList.GetEnumerator())
+            {
+                while (enumator.MoveNext())
+                {
+                    Console.WriteLine(enumator.Current);
+                }
+            }
+            */
+        }
+    }
+}
